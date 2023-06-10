f485f7d4e67a7c239d7ce13c53b60233e3f825da
linh ken <linhken07102002@gmail.com>
2023-06-11 02:17:00 +0700

    update

---
+++ nodejs/src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import routerProduct from "./routers/product";
+import routerCategory from "./routers/category";
+import routerPromotion from "./routers/promotion";
+import routerAuth from "./routers/auth";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+mongoose.connect("mongodb://127.0.0.1:27017/hoanghamobile");
+app.use("/api", routerProduct);
+app.use("/api", routerCategory);
+app.use("/api", routerPromotion);
+app.use("/api", routerAuth);
+
+export const viteNodeApp = app;
